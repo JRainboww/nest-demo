bc23cb4f23e7d2ba90059cd11100ff5e6e13e296
AudreyHep <1293157825@qq.com>
2019-08-05 23:24:51 +0800

    change dir structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './controller/app.controller';
-import { CatsController } from './controller/cats.controller';
-import { AppService } from './service/app.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
+  imports: [CatsModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { CreateCatDto} from './dto/create-cat.dto';
+import { Cat } from './interface/cat.interface';
+import { CatsService } from './cats.service'
+
+@Controller('cats')
+export class CatsController {
+
+  constructor(private readonly catsService: CatsService) {}
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    return this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
+  }
+}
