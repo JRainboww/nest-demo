58f70a7cd76f4425b8fa89a5970b4f38f1b11b46
AudreyHep <1293157825@qq.com>
2019-08-08 00:31:05 +0800

    fix

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,6 @@
-import { Controller, Get, UseGuards, Request, Post } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request, Post, Inject } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { AuthService } from '../auth/auth.service';
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly authService: AuthService) {}
-
-    @UseGuards(AuthGuard('local'))
-    @Post('login')
-    async login(@Request() req) {
-        return this.authService.login(req.user);
-    }
-
-    @UseGuards(AuthGuard('jwt'))
-    @Get('me')
-    getProfile(@Request() req) {
-        return req.user;
-    }
 }
